9fb7dc31e44bbc61746613792a94e446cfed5d14
human8man <2311085@i-seifu.jp>
2024-12-18 11:55:28 +0900

    かめら修正

--- ACC/Source/GameObject/Camera/CCamera.h
+++ ACC/Source/GameObject/Camera/CCamera.h
@@ -17,8 +17,10 @@ public:
 		Back,
 	};
 
+	// WND_WM(M = MID)は全体で使わなさそうなのでここで宣言.
 	static constexpr int WND_HM = WND_H / 2;
 	static constexpr int WND_WM = WND_W / 2;
+	static constexpr POINT RESETPOS = { WND_HM, WND_WM };
 public:
 	CCamera();
 	~CCamera();
@@ -43,38 +45,26 @@ public:
 
 private:
 	void KeyInput();					// キー入力.
-	void MouseMath();					// マウス移動量の計算.
-	void MousePosReset();				// マウス位置の初期化.
 	void CameraMove(int vec);			// カメラの移動.
 	void MouseMove(D3DXVECTOR2 value);	// マウスの移動量に応じてカメラの方向を変更.
 
 private:
-	CAMERA			m_Camera;			// カメラ情報.
-	float			m_ViewAngle;		// 視野角.
+	CAMERA	m_Camera;	// カメラ情報.
 
-	D3DXVECTOR3		m_DirectionPos;		// カメラ位置の移動量.
-	D3DXVECTOR3		m_Playerpos;		// 外部からのプイレイヤー座標を保存する用.
+	POINT	m_NowCurorPos;		// 現在のマウスの位置.
+	POINT	m_BeforCursorPos;	// 過去のマウスの位置.
 
-	//マウス位置情報.
-	POINT			m_NowCurorPos;		// 現在のマウスの位置.
-	POINT			m_BeforCursorPos;	// 過去のマウスの位置.
-	 
-	float			m_MoveValue;		// 移動量.
+	D3DXVECTOR2 m_MouseMoveDis;		// マウスの移動した距離.
+	D3DXVECTOR3 m_CameraRot;		// カメラの回転軸.
+	D3DXVECTOR3 m_Playerpos;		// プイレイヤー座標.
+	D3DXVECTOR3 m_DirectionPos;		// カメラ座標～注視点のベクトル.
 
-	float			m_CameraYaw;		// カメラ角度x軸.
-	float			m_CameraPitch;		// カメラ角度y軸.
-	D3DXVECTOR3		m_CameraRot;		// カメラの回転軸.
+	float	m_ViewAngle;	// 視野角.
+	float	m_MoveValue;	// 移動量.
+	float	m_CameraYaw;	// カメラ角度x軸.
+	float	m_CameraPitch;	// カメラ角度y軸.
+	float	m_MouseSens;	// マウス感度.
+	float	m_DefaultFov;	// デフォルトの視野角.
 
-	D3DXVECTOR2		m_MouseMoveDis;		// マウスの移動した距離.
-	float			m_MouseSens;		// マウス感度.
-	bool			m_CanMoveMouse;		// マウスが自由に移動できるかどうか.
-
-	float m_DefaultFov; // デフォルトの視野角.
-
-	// デバッグカメラ用.
-	POINT m_MousePos;	 // 今のマウス位置.
-	POINT m_OldMousePos; // 前のマウス位置.
-
-	D3DXVECTOR3 m_MouseMoveValue;	// マウスの移動量.
-	D3DXVECTOR3 m_LookMoveValue;	// 注視点の移動量.
+	bool	m_CanMoveMouse;	// マウスが自由に移動できるかどうか.
 };
